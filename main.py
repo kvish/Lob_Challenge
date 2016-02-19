69f66625c578149d9bc3ef3752d8c53e256e45cc
Karan Vishwanathan <kvish@Karans-MBP.attlocal.net>
2016-02-19 02:39:27 -0600

    working letter demo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import request, render_template, redirect, url_for, redirect
 import json
+import lob
+import urllib
+import urllib2
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -13,7 +16,8 @@ def index():
 
 @app.route('/send', methods = ['POST'])
 def send():
-	#return "send letter"
+	
+	#access form data
 	name = request.form['name']
 	address1 = request.form['address1']
 	address2 = request.form['address2']
@@ -22,8 +26,42 @@ def send():
 	zipcode = request.form['zip']
 	letter = request.form['letter']
 
-	return name + address1 + address2 + city + state + zipcode + letter
+
+	if name == '' or address1 == '' or address2 == '' or city == '' or state == '' or zipcode == '':
+		return '<html><h1> Something is wrong with your request! make sure all fields are filled </h1></html>'
 	
+	#make api requests
+	google_api_key = 'AIzaSyB7Ei3b6IIMhv1udluKomljDwBcJeZSIgQ'
+	lob.api_key = 'test_37e4de5759d46d675a12a7819f89fb08145'
+
+
+	letter = lob.Letter.create(
+	  description = 'Demo Letter',
+	  to_address = {
+	      'name': 'Harry Zhang',
+	      'address_line1': '123 Test Street',
+	      'address_city': 'Mountain View',
+	      'address_state': 'CA',
+	      'address_zip': '94041',
+	      'address_country': 'US'
+	  },
+	  from_address = {
+	      'name': 'Harry Zhang',
+	      'address_line1': '123 Test Avenue',
+	      'address_city': 'Seattle',
+	      'address_state': 'WA',
+	      'address_zip': '94041',
+	      'address_country': 'US'
+	  },
+	  file = '<html style="padding-top: 3in; margin: .5in;">HTML Letter for {{name}}</html>',
+	  data = {
+	    'name': 'Harry'
+	  },
+	  color = True
+	)
+
+
+	return redirect(letter['url'])
 
 
 if __name__ == '__main__':
